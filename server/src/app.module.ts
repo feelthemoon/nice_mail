73f2d6716442ace3d2ba4043203daf52af2d9dda
Vladislav Vdovin <programmer28.vdovin@yandex.ru>
2022-05-12 16:40:17 +0300

    refactor: backend

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,4 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { TempmailModule } from './tempmail/tempmail.module';
 import { TempmailRestMiddleware } from './tempmail/rest/tempmail.rest.middleware';
@@ -8,16 +7,6 @@ import { TempmailRestMiddleware } from './tempmail/rest/tempmail.rest.middleware
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
-    }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.PG_HOST,
-      port: parseInt(process.env.PG_PORT),
-      username: process.env.PG_USERNAME,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DATABASE,
-      entities: [],
-      synchronize: true,
     }),
     TempmailModule,
   ],
